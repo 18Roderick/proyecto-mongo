1a480994e08892ff7f1fe06dbfadf3415ecc1ea9
18Roderick <rjrr507@gmail.com>
2018-03-20 23:56:42 -0600

    agregandp app.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # proyecto-mongo
+### 
 "https://www.ebi.ac.uk/proteins/api/proteins?offset=1&size=100&organism=Nicotiana%20tabacum"
--- app.js
+++ app.js
@@ -1,23 +1,51 @@
-const http = require('http');
+'use strict'
+const mongoClient = require('mongodb').MongoClient,
+	url = 'mongodb://localhost:27017/',
+	dbName = 'GenomicDB',
+	collectionName = 'Proteinas';
 const request = require('request');
-var urls = [
-	"https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=2&organism=Nicotiana%20tabacum",
-	"https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=2&organism=Escherichia%20coli",
-	"https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=2&organism=Cannabis%20sativa",
-	"https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=2&organism=Botryococcus%20braunii"
-]
+var link = "https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=1&organism=Nicotiana%20tabacum"
 var options = {
-	url : urls[0],
-	headers:{
+	url: link,
+	headers: {
 		"Accept": "application/json"
 	}
+};
+
+function gettingData(data) {
+
 }
 
 request(options, (error, response, body) => {
-	console.log('error:', error); // Print the error if one occurred
-	//console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	console.log('body:', body); // Print the HTML for the Google homepage.
-	body.forEach( (data ) => {
-		console.log(data)
-	})
+	let data = JSON.parse(body);
+	let lista = []
+	let dataset = {};
+	data.forEach(element => {
+		for (const i in element) {
+			//console.log(element[i])
+			if (i === 'protein') {
+				for (const key in element[i]) {
+					if (key === "submittedName") {
+						dataset.CadenaDNA = element['sequence']['sequence'];
+						//console.log(element[i][key]);
+						if (typeof (element[i][key]) == 'array') {
+							dataset.Organismo = element[i][key][0]['fullName']['value'];
+							dataset.Funcion = element[i][key][0]['fullName']['evidences']['source']['url'];
+							
+						} else {
+							console.log('Sumited name ############',data);
+						}
+					} else if (key === "recommendedName") {
+						//console.log(element[i][key]);
+						dataset.Organismo = element[i][key]['fullName']['value'];
+						dataset.Funcion = element['comments'][0]['text'][0]['value'];
+						dataset.CadenaDNA = element['sequence']['sequence'];
+
+					}
+				}
+			}
+		};
+		lista.push(dataset);
+		console.log(dataset)
+	});
 })
