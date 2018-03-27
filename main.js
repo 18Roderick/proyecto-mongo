c7bf448dc1ca62692eccad061327ff736297faac
18Roderick <rjrr507@gmail.com>
2018-03-27 03:53:49 -0600

    cambios

--- main.js
+++ main.js
@@ -18,6 +18,19 @@ function dbConnection() {
 	});
 };
 
+function findeSome(db, dato) {
+	var dbo = db.db(dbName);
+	dbo.collection(collectionName).find(dato)
+}
+
+function dbInsert(db, dato) {
+    var dbo = db.db(dbName);
+    dbo.collection(collectionName).insertOne(dato, (err, res) => {
+        if (err) throw err;
+        console.log("documento insertado");
+    });
+
+}
 
 function dbInsertMany(db, dato) {
 	var dbo = db.db(dbName);
@@ -28,8 +41,8 @@ function dbInsertMany(db, dato) {
 	});
 }
 
-function gettingData(options){
-	return new Promise( (resolve, reject ) => {
+function gettingData(options) {
+	return new Promise((resolve, reject) => {
 		return request(options, (error, response, body) => {
 			return (error) ? reject(new error('problemas al consultar el api')) : resolve(JSON.parse(body));
 		})
@@ -46,25 +59,68 @@ function requestOptions(start = 0, size = 100) {
 	return opciones;
 }
 
+function procces(data) {
+	let lista = []
+
+	data.forEach(element => {
+		let dataset = {};
+		for (const i in element) {
+			//console.log(element[i])
+			if (i === 'protein') {
+				for (const key in element[i]) {
+					if (key === "submittedName") {
+
+						//console.log(element[i][key]);
+						if (typeof(element[i][key]) == 'array') {
+							dataset.Organismo = element[i][key][0]['fullName']['value'];
+							dataset.Funcion = element[i][key][0]['fullName']['evidences']['source']['url'];
+
+						} else {
+							dataset.Organismo = element[i][key][0]['fullName']['value'];
+							dataset.Funcion = element[i][key][0]['fullName']['evidences'][0]['source']['url'];
+						}
+						dataset.CadenaDNA = element['sequence']['sequence'];
+
+					} else if (key === "recommendedName") {
+						//console.log(element[i][key]);
+						dataset.Organismo = element[i][key]['fullName']['value'];
+						dataset.Funcion = element['comments'][0]['text'][0]['value'];
+						dataset.CadenaDNA = element['sequence']['sequence'];
+
+					}
+				}
+			}
+		};
+		lista.push(dataset);
+		//console.log("Imprimiendo datset  \n\n", dataset)
+
+	});
+
+	return new Promise((resolve, reject) => {
+		return (lista.length > 0) ? resolve(lista) : reject(new error('Lista de proteinas vacia'))
+	})
+}
+
+
+async function main(start = 0, size = 1) {
+	console.log('Temporizador corriendo ');
+	try {
+
+		const data = await gettingData(requestOptions(start, size));
+		const registros = await procces(data);
+		console.log(registros);
+		setTimeout(() => {
+			let tmpStart = start;
+			start = size + tmpStart;
+			console.log(start, size);
+			//main()
+		}, 10000);
+
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+main();
 
 console.log(requestOptions());
-(
-	async function() {
-		console.log('Temporizador corriendo ');
-		try{
-			dbConnection()
-			 	.then(db => {
-			  		console.log('conexion exitosa');
-			  		db.close()
-			  	})
-			gettingData(requestOptions(0, 2))
-				.then( data => {
-					console.log(data)
-				})
-
-		}catch(error){
-			console.log(error);
-		}
-	}
-
-());
